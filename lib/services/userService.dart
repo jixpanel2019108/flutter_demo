b171aa7dd7045292f8ec9cadb2239854d7b82d81
Allan Hernandez <ahernandez-2019064@kinal.edu.gt>
2021-09-01 18:51:00 -0600

    Intentos de alertas y variables

--- lib/services/userService.dart
+++ lib/services/userService.dart
@@ -1,7 +1,6 @@
 // @dart=2.9
 
 import 'dart:async';
-import 'dart:ffi';
 import 'dart:js';
 
 import 'package:flutter/material.dart';
@@ -77,6 +76,7 @@ class UserService{
   }
   
   Future <void> listPerfilService(String token) async {
+
     final http.Response response = await http.post(Uri.parse(url+"/listPerfil"),
                                     headers: <String,String>{ "Content-Type": "application/json"},
                                     body: jsonEncode({"key": "12345","token": token}));
