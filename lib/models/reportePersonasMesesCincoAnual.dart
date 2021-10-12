8db3b4caf1041269b85defef30fbc2c8a1d4b565
Jonatan W <josuejonatan142@gmail.com>
2021-10-12 10:35:32 -0600

    ReportePersonasMesesCincoAnual model and service

---
+++ lib/models/reportePersonasMesesCincoAnual.dart
@@ -0,0 +1,66 @@
+//@dart=2.9
+// To parse this JSON data, do
+//
+//     final reportePersonasMesesCincoAnual = reportePersonasMesesCincoAnualFromJson(jsonString);
+
+import 'dart:convert';
+
+ReportePersonasMesesCincoAnual reportePersonasMesesCincoAnualFromJson(String str) => ReportePersonasMesesCincoAnual.fromJson(json.decode(str));
+
+String reportePersonasMesesCincoAnualToJson(ReportePersonasMesesCincoAnual data) => json.encode(data.toJson());
+
+class ReportePersonasMesesCincoAnual {
+    ReportePersonasMesesCincoAnual({
+        this.msg,
+        this.total,
+        this.code,
+        this.listado,
+        this.error,
+    });
+
+    String msg;
+    String total;
+    String code;
+    List<Listado> listado;
+    bool error;
+
+    factory ReportePersonasMesesCincoAnual.fromJson(Map<String, dynamic> json) => ReportePersonasMesesCincoAnual(
+        msg: json["msg"],
+        total: json["total"],
+        code: json["code"],
+        listado: List<Listado>.from(json["listado"].map((x) => Listado.fromJson(x))),
+        error: json["error"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "msg": msg,
+        "total": total,
+        "code": code,
+        "listado": List<dynamic>.from(listado.map((x) => x.toJson())),
+        "error": error,
+    };
+}
+
+class Listado {
+    Listado({
+        this.entradas,
+        this.mes,
+        this.year,
+    });
+
+    String entradas;
+    String mes;
+    String year;
+
+    factory Listado.fromJson(Map<String, dynamic> json) => Listado(
+        entradas: json["entradas"],
+        mes: json["mes"],
+        year: json["AÃ±o"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "entradas": entradas,
+        "mes": mes,
+        "año": year,
+    };
+}
